73ee37cb31cc9f66e0408088055eaf451f4a7bc8
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2011-05-11 16:46:21 +0200

    Added db-handler spec, not finished yet.

---
+++ test/db-handler-spec.js
@@ -0,0 +1,24 @@
+var dbHandler = require('../lib/db-handler.js');
+var db;
+
+describe("A database handler", function() {
+  it("should be able to prepare the database", function() {
+    // pending
+  });
+  
+  it("should be able to insert a job", function() {
+    // pending
+  });
+  
+  it("should be able to find a job", function() {
+    // pending
+  });
+  
+  it("should be able to update a job", function() {
+    // pending
+  });
+  
+  it("should be able to close the database", function() {
+    // pending
+  });
+});
